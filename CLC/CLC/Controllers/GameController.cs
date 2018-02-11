2ab31ad4cfdc63bfa883d2a8af5b58a5234f992c
Connor Low <clow2@my.gcu.edu>
2018-02-10 17:11:00 -0700

    Game, GameController, Cell

---
+++ CLC/CLC/Controllers/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CLC.Models;
+using CLC.Services.Business;
+
+namespace CLC.Controllers
+{
+    public class GameController : Controller
+    {
+        public static Game GameLogic { get; set; }
+
+        /**
+         * Returns the size-selection view
+         */
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Select(Game game)
+        {
+            GameLogic = game;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Play(Cell cell)
+        {
+            return View();
+        }
+    }
+}
---
+++ CLC/CLC/Models/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CLC.Models
+{
+    public class Cell
+    {
+        public bool Mine { set; get; }
+        public bool Flagged { get; set; } = false;
+        public bool Checked { set; get; } = false;
+        public int Adjacent { get; set; } = 0;
+
+        public Cell(bool isLive)
+        {
+            Mine = isLive;
+        }
+    }
+}
