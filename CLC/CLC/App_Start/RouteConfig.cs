b3fdc0bae2596ae0c975c0234f62df811b5d83b6
Connor Low <clow2@my.gcu.edu>
2018-02-11 20:45:31 -0700

    looking good, good lookin

--- CLC/CLC/App_Start/RouteConfig.cs
+++ CLC/CLC/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace CLC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Default", action = "Index", id = UrlParameter.Optional}
+                "Default",
+                "{controller}/{action}/{id}",
+                new {controller = "Default", action = "Index", id = UrlParameter.Optional}
             );
 
             
