543f7175bcf462bf648078ed42854301017f50e2
Connor Low <clow2@my.gcu.edu>
2018-02-28 15:39:11 -0700

    calculator example for AJAX

---
+++ DEFINITELY NOT THE QUIZ CODE WHY DO YOU ASK/Quiz1/Models/CalculatorModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Quiz1.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        [DisplayName("Left value")]
+        [DefaultValue("")]
+        public double Left { get; set; }
+
+        [Required]
+        [DisplayName("Right value")]
+        [DefaultValue("")]
+        public double Right { get; set; }
+
+        [Required]
+        [DisplayName("Operation")]
+        [DefaultValue("")]
+        public string Action { get; set; }
+
+        [Required]
+        [DisplayName("Result")]
+        [DefaultValue("")]
+        public double Result { get; set; }
+    }
+}
---
+++ DEFINITELY NOT THE QUIZ CODE WHY DO YOU ASK/Quiz1/Services/CalculatorService.cs
@@ -0,0 +1,39 @@
+﻿using Quiz1.Models;
+
+namespace Quiz1.Services
+{
+    public class CalculatorService
+    {
+        public CalculatorService(CalculatorModel model)
+        {
+            Model = model;
+        }
+
+        public CalculatorModel Model { get; set; }
+
+        public CalculatorModel Calculate()
+        {
+            switch (Model.Action)
+            {
+                case "add":
+                    Model.Result = Model.Left + Model.Right;
+                    Model.Action = "+";
+                    break;
+                case "subtract":
+                    Model.Result = Model.Left - Model.Right;
+                    Model.Action = "-";
+                    break;
+                case "multiply":
+                    Model.Result = Model.Left * Model.Right;
+                    Model.Action = "*";
+                    break;
+                case "divide":
+                    Model.Result = Model.Left / Model.Right;
+                    Model.Action = "/";
+                    break;
+            }
+
+            return Model;
+        }
+    }
+}
