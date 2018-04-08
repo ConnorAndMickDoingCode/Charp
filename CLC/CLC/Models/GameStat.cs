2ef4ec71404f093ef9e0e6b548b21f79201e7ef2
Connor Low <clow2@my.gcu.edu>
2018-04-08 07:39:37 -0700

    Started on REST service

---
+++ CLC/CLC/Models/GameStat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CLC.Models
+{
+    [DataContract]
+    public class GameStat
+    {
+        [DataMember] public int Id { get; set; }
+        [DataMember] public int Time { get; set; }
+        [DataMember] public string Size { get; set; }
+        [DataMember] public string Username { get; set; }
+    }
+}
---
+++ CLC/MinesweeperStatsService/DTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+using CLC.Models;
+
+namespace MinesweeperStatsService
+{
+    [DataContract]
+    public class DTO
+    {
+        public DTO(int status, string message, List<GameStat> data)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+
+        [DataMember] public int Status { get; set; }
+        [DataMember] public string Message { get; set; }
+        [DataMember] public List<GameStat> Data { get; set; }
+    }
+}
