ed3d5136f0ac6d2e4e6f3d3d0e6578fa7c95b46e
mick torres <micklunatorres9@gmail.com>
2018-04-18 16:25:40 -0700

    logging stuff
    lumberjack buttcrack

---
+++ CLC/CLC/Services/Utility/TheLogger.cs
@@ -0,0 +1,60 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CLC.Services.Utility
+{
+    public class TheLogger : ILogger
+    {
+
+        private TheLogger logger;
+        public Logger Logger;
+
+        public Logger GetLogger()
+        {
+            return LogManager.GetLogger("LoggerRules");
+        }
+
+        public void Debug(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger().Debug(message);
+            else
+            {
+                GetLogger().Debug(message, arg);
+            }
+        }
+
+        public void Error(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger().Error(message);
+            else
+            {
+                GetLogger().Error(message, arg);
+            }
+        }
+
+        public void Info(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger().Info(message);
+            else
+            {
+                GetLogger().Info(message, arg);
+            }
+        }
+
+        public void Warning(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger().Warn(message);
+            else
+            {
+                GetLogger().Warn(message, arg);
+            }
+        }
+    }
+}
